c70e952ac90788ecd626484a002d8fc3db6f3cbe
HAN-SEOHYUN <000312>
2022-08-03 00:34:45 +0900

    mustache=>thymeleaf 변경 !!

--- src/main/java/com/proj/KnitMarket/KnitMarketApplication.java
+++ src/main/java/com/proj/KnitMarket/KnitMarketApplication.java
@@ -2,8 +2,9 @@ package com.proj.KnitMarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class KnitMarketApplication {
 
 	public static void main(String[] args) {
