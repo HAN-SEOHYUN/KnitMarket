a71c6953ee807b16b2f8cbda8b275a012caddea3
HAN-SEOHYUN <000312>
2022-08-03 00:34:45 +0900

    mustache=>thymeleaf 변경 !!

--- src/main/java/com/proj/KnitMarket/domain/User/User.java
+++ src/main/java/com/proj/KnitMarket/domain/User/User.java
@@ -18,37 +18,22 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable=false)
-    private Role role;
-
     @Column
-    private String ph;
+    private String hp;
     private String email;
     private String addressId;
+    private String accountBank;
     private String accountNo;
 
+
     @Builder
-    public User(long id, String name, Role role, String ph, String email, String addressId, String accountNo) {
+    public User(long id, String name, String hp, String email, String addressId, String accountBank, String accountNo) {
         this.id = id;
         this.name = name;
-        this.role = role;
-        this.ph = ph;
+        this.hp = hp;
         this.email = email;
         this.addressId = addressId;
+        this.accountBank = accountBank;
         this.accountNo = accountNo;
-    }
-
-    public User update(String name, String ph, String email, String accountNo) {
-        this.name = name;
-        this.ph = ph;
-        this.email = email;
-        this.accountNo = accountNo;
-
-        return this;
-    }
-
-    public String getRoleKey() {
-        return this.role.getKey();
     }
 }
