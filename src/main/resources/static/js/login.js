4cc687232e47489a5c2fc39b7e4ee51fb6c9fcce
HAN-SEOHYUN <000312>
2022-08-11 22:45:30 +0900

    js user/seller 나누기 ok

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,4 +1,19 @@
-$(function (){
+$(function () {
+
+
+    $('.login-btn a').click(function(){
+        var role = "";
+        if ($('#checkbox_user').is(":checked")) {
+            role = "user";
+        } else {
+            role = "seller";
+        }
+        var kakaoUrl = "https://kauth.kakao.com/oauth/authorize?" +
+            "client_id=0a9af639b0425e40c10ad4d291ce4637" +
+            "&redirect_uri=http://localhost:8086/knitmarket/kakaoLogin/requestToken_" + role +
+        "&response_type=code";
+        location.href = kakaoUrl;
+    });//한개만 선택되게하기
 
 
 });
