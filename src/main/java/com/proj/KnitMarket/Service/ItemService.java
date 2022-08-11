659c60a148cd6d1c9039c4215d239025b7a2d908
HAN-SEOHYUN <000312>
2022-08-12 02:11:49 +0900

    item seller 함께 업로드 ok

--- src/main/java/com/proj/KnitMarket/Service/ItemService.java
+++ src/main/java/com/proj/KnitMarket/Service/ItemService.java
@@ -1,6 +1,9 @@
 package com.proj.KnitMarket.Service;
 
+import com.proj.KnitMarket.domain.Item.Item;
 import com.proj.KnitMarket.domain.Item.ItemRepository;
+import com.proj.KnitMarket.domain.Member.Seller;
+import com.proj.KnitMarket.domain.Member.SellerRepository;
 import com.proj.KnitMarket.dto.ItemRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +15,17 @@ import javax.transaction.Transactional;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final SellerRepository sellerRepository;
 
     //@Transactional : db 트랜잭션 자동으로 commit 해줌
     @Transactional //아이템 등록
-    public Long save(ItemRequestDto itemDto){
+    public Long save(ItemRequestDto itemDto,String email){
+        Seller seller = sellerRepository.findByEmail(email);
+
+        itemDto.setSeller(seller);
+
+        Item item = itemDto.toEntity();
+
         return itemRepository.save(itemDto.toEntity()).getId();
     }
 
--- src/main/java/com/proj/KnitMarket/dto/SellerResponseDto.java
+++ src/main/java/com/proj/KnitMarket/dto/SellerResponseDto.java
@@ -3,17 +3,22 @@ package com.proj.KnitMarket.dto;
 import com.proj.KnitMarket.domain.Member.Seller;
 import lombok.Getter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 public class SellerResponseDto {
     private Long id;
     private String email;
     private String name;
+    private List<ItemResponseDto> items;
 
     //entity to DTO
     public SellerResponseDto(Seller entity){
         this.id=entity.getId();
         this.email=entity.getEmail();
         this.name=entity.getName();
+        this.items = entity.getItem().stream().map(ItemResponseDto::new).collect(Collectors.toList());
     }
 
 }
