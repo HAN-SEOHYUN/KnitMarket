c9e82f8b268dcec3fc9ed4feff2ea00711792a40
HAN-SEOHYUN <000312>
2022-08-13 00:09:20 +0900

    파일업로드 ing

---
+++ src/main/java/com/proj/KnitMarket/domain/Item/FileEntity.java
@@ -0,0 +1,37 @@
+package com.proj.KnitMarket.domain.Item;
+
+import com.proj.KnitMarket.domain.Member.Seller;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="file")
+@Entity
+public class FileEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orginFileName;
+    private String fileName;
+    private String filePath;
+
+    @OneToOne
+    @JoinColumn(name="itme_id")
+    private Item item; //파일
+
+    @Builder
+    public FileEntity(Long id, String orginFileName, String fileName, String filePath, Item item) {
+        this.id = id;
+        this.orginFileName = orginFileName;
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.item = item;
+    }
+}
---
+++ src/main/java/com/proj/KnitMarket/dto/FileResponseDto.java
@@ -0,0 +1,22 @@
+package com.proj.KnitMarket.dto;
+
+import com.proj.KnitMarket.domain.Item.FileEntity;
+import lombok.Getter;
+
+@Getter
+public class FileResponseDto {
+    private Long id;
+    private String orginFileName;
+    private String fileName;
+    private String filePath;
+    private Long itemId;
+
+    public FileResponseDto(FileEntity entity){
+        this.id=entity.getId();
+        this.orginFileName = entity.getOrginFileName();
+        this.fileName = entity.getFileName();
+        this.filePath = entity.getFilePath();
+        this.itemId=entity.getItem().getId();
+    }
+
+}
