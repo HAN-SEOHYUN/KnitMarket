74fe22b9769accfb7c078b35ba4c4a83581ba6b6
HAN-SEOHYUN <000312>
2022-08-17 04:05:30 +0900

    상품목록 OK / 이미지불러오기까지 OK

--- src/main/java/com/proj/KnitMarket/Service/ItemService.java
+++ src/main/java/com/proj/KnitMarket/Service/ItemService.java
@@ -8,10 +8,13 @@ import com.proj.KnitMarket.domain.Member.Seller;
 import com.proj.KnitMarket.domain.Member.SellerRepository;
 import com.proj.KnitMarket.dto.FileRequestDto;
 import com.proj.KnitMarket.dto.ItemRequestDto;
+import com.proj.KnitMarket.dto.ItemResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -26,17 +29,35 @@ public class ItemService {
     @Transactional //아이템 등록
     public Long save(ItemRequestDto itemDto, String email, FileRequestDto fileRequestDto){
         Seller seller = sellerRepository.findByEmail(email);
-
         FileEntity file = fileService.save(fileRequestDto); // file 저장
 
         itemDto.setFileEntity(file);
         itemDto.setSeller(seller);
-
         Item item = itemDto.toEntity();
 
         return itemRepository.save(itemDto.toEntity()).getId();
     }
 
     // index 상품목록
+    @Transactional
+    public List<ItemResponseDto> getItemList(){
+        List<Item> items = itemRepository.findAll();
+        List<ItemResponseDto> itemDtoList = new ArrayList<>();
+
+        for(Item item : items){
+            ItemResponseDto responseDto = ItemResponseDto.builder()
+                    .id(item.getId())
+                    .itemName(item.getItemName())
+                    .itemDesc(item.getItemDesc())
+                    .orginFileName(item.getFile().getOrginFileName())
+                    .price(item.getPrice())
+                    .sellerName(item.getSeller().getName())
+                    .build();
+
+            itemDtoList.add(responseDto);
+        }//for
+
+        return itemDtoList;
+    }
 
 }
