abbf87787c586e49ddb570e8fb7679f8358a1ee9
HAN-SEOHYUN <000312>
2022-08-17 00:51:04 +0900

    sellStatus 추가 ok / 상품목록 css 수정

--- src/main/java/com/proj/KnitMarket/dto/ItemRequestDto.java
+++ src/main/java/com/proj/KnitMarket/dto/ItemRequestDto.java
@@ -1,25 +1,35 @@
 package com.proj.KnitMarket.dto;
 
+import com.proj.KnitMarket.Constant.SellStatus;
 import com.proj.KnitMarket.domain.Item.FileEntity;
 import com.proj.KnitMarket.domain.Item.Item;
 import com.proj.KnitMarket.domain.Member.Seller;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 public class ItemRequestDto {
+
+    @NotBlank(message = "상품명은 필수 입력 값입니다")
     private String itemName;
+
+    @NotNull(message = "가격은 필수 입력 값입니다")
     private int price;
+
     private String itemDesc;
     private Seller seller;
+
+    @NotNull(message = "상품이미지는 필수 입력 값입니다")
     private MultipartFile file;
     private FileEntity fileEntity;
 
-
-    //private SellStatus sellStatus;
+    private SellStatus sellStatus;
 
 //dto => entity
     public Item toEntity() {
@@ -29,17 +39,18 @@ public class ItemRequestDto {
                 .itemDesc(itemDesc)
                 .seller(seller)
                 .file(fileEntity)
+                .sellStatus(sellStatus)
                 .build();
     }
 
-
     @Builder
-    public ItemRequestDto(String itemName, String itemDesc, int price, Seller seller, FileEntity file) {
+    public ItemRequestDto(String itemName, String itemDesc, int price, Seller seller, FileEntity file,SellStatus sellStatus) {
         this.price = price;
         this.itemDesc = itemDesc;
         this.itemName = itemName;
         this.seller = seller;
         this.fileEntity = file;
+        this.sellStatus = sellStatus;
     }
 
 }
