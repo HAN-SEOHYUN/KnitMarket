f9a6e14c9a1a6ac02a3ce0d2401129b3aac41c30
HAN-SEOHYUN <000312>
2022-08-20 03:45:31 +0900

    수정정보 DB Update OK

--- src/main/java/com/proj/KnitMarket/Service/FileService.java
+++ src/main/java/com/proj/KnitMarket/Service/FileService.java
@@ -5,6 +5,7 @@ import com.proj.KnitMarket.domain.Item.FileEntityRepository;
 import com.proj.KnitMarket.domain.Item.Item;
 import com.proj.KnitMarket.domain.Item.ItemRepository;
 import com.proj.KnitMarket.dto.FileRequestDto;
+import com.proj.KnitMarket.dto.FileResponseDto;
 import com.proj.KnitMarket.dto.ItemRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,20 @@ public class FileService {
 
     @Transactional
     public FileEntity save(FileRequestDto fileRequestDto){
-
         return fileRepository.save(fileRequestDto.toEntity());
+    }
+
+    @Transactional
+    public FileResponseDto getFile(Long id){
+        FileEntity fileEntity = fileRepository.findById(id).get();
+
+        FileResponseDto fileResponseDto = FileResponseDto.builder()
+                .id(id)
+                .orginFileName(fileEntity.getOrginFileName())
+                .filePath(fileEntity.getFilePath())
+                .build();
+
+        return  fileResponseDto;
     }
 
 }
--- src/main/java/com/proj/KnitMarket/domain/Item/ItemRepository.java
+++ src/main/java/com/proj/KnitMarket/domain/Item/ItemRepository.java
@@ -3,6 +3,6 @@ package com.proj.KnitMarket.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item,Long>{
-
+    Item findItemById(Long id);
 
 }
