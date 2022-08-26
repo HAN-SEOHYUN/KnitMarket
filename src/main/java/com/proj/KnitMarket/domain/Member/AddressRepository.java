4f694c37b8af59c28521f9b9bc23eff7b0bd536f
HAN-SEOHYUN <000312>
2022-08-27 05:08:44 +0900

    mypage 틀잡기

---
+++ src/main/java/com/proj/KnitMarket/domain/Member/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.proj.KnitMarket.domain.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+
+
+}
