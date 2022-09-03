095fd3c92992a9adae530486762200f67a122d80
HAN-SEOHYUN <000312>
2022-09-03 19:16:14 +0900

    주문하기 엔티티설계 ing ~ 2

---
+++ src/main/java/com/proj/KnitMarket/dto/OrderItemDto.java
@@ -0,0 +1,32 @@
+package com.proj.KnitMarket.dto;
+
+import com.proj.KnitMarket.domain.Item.Item;
+import com.proj.KnitMarket.domain.Order.Order;
+import com.proj.KnitMarket.domain.Order.OrderItem;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OrderItemDto {
+
+    private Long id;
+    private Item item;
+    private Order order;
+
+    public OrderItem toEntity(){
+        return OrderItem.builder()
+                .id(id)
+                .order(order)
+                .item(item)
+                .build();
+    }
+
+    @Builder
+    public OrderItemDto(Long id, Item item, Order order) {
+        this.id = id;
+        this.item = item;
+        this.order = order;
+    }
+}
