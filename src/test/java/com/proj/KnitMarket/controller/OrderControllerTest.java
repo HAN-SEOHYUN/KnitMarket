aecab66c34831d3dc20ee9e2c341d776e5a144cb
HAN-SEOHYUN <000312>
2022-09-05 21:22:25 +0900

    장바구니 전체 목록 주문하기 OK

---
+++ src/test/java/com/proj/KnitMarket/controller/OrderControllerTest.java
@@ -0,0 +1,37 @@
+package com.proj.KnitMarket.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.proj.KnitMarket.Service.OrderService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.HttpSession;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+}
