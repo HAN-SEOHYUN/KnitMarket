45ad2d7fd208a6ac2eb7e8a42b1a634830758523
HAN-SEOHYUN <000312>
2022-09-06 17:30:12 +0900

    개별상품 주문 전 리스트 띄우기 OK

--- src/main/java/com/proj/KnitMarket/controller/OrderController.java
+++ src/main/java/com/proj/KnitMarket/controller/OrderController.java
@@ -3,6 +3,9 @@ package com.proj.KnitMarket.controller;
 import com.proj.KnitMarket.Service.CartService;
 import com.proj.KnitMarket.Service.OrderService;
 import com.proj.KnitMarket.domain.Order.Order;
+import com.proj.KnitMarket.domain.Order.OrderItem;
+import com.proj.KnitMarket.dto.OrderDto;
+import com.proj.KnitMarket.dto.OrderItemDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,20 +25,20 @@ public class OrderController {
     private final OrderService orderService;
     private final CartService cartService;
 
-    @GetMapping("/order/{orderId}")
+    /*@GetMapping("/order/{orderId}")
     public String order_get(RedirectAttributes rttr) {
         Long orderId = (Long)rttr.getAttribute("orderId");
         log.info("requestParam orderId={}",orderId);
         return "user/order";
-    }
+    }*/
 
     //단일상품주문
     @GetMapping("/order/{itemId}")
-    public String order_item_get(@PathVariable("itemId")Long itemId, HttpSession httpSession, RedirectAttributes rttr){
+    public String order_item_get(@PathVariable("itemId")Long itemId, HttpSession httpSession, Model model){
         String email = (String) httpSession.getAttribute("email");
-        Order order = orderService.order(itemId, email);
-        rttr.addAttribute("orderId",order.getId());
-        return "forward:/knitmarket/order";
+        List<OrderItemDto> orderItemDtoList = orderService.order(itemId, email);
+        model.addAttribute("orderList",orderItemDtoList);
+        return "user/order";
     }
 
     //장바구니상품주문
