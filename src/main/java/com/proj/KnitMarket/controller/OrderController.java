03cc17e88ecc2b61df64091ad0e018f4109d8bbc
HAN-SEOHYUN <000312>
2022-09-08 01:51:04 +0900

    결제 후 상태변경 및 장바구니 비우기 설정 OK

--- src/main/java/com/proj/KnitMarket/controller/OrderController.java
+++ src/main/java/com/proj/KnitMarket/controller/OrderController.java
@@ -34,7 +34,6 @@ import java.util.Map;
 public class OrderController {
 
     private final RestTemplate restTemplate = new RestTemplate();
-
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     private final OrderService orderService;
@@ -87,16 +86,11 @@ public class OrderController {
     //결제
     //http://localhost:8086/success?orderId=1234567&paymentKey=vG45eDbZnodP9BRQmyarYBdE4boBL8J07KzLNkE6AOMwXYWl&amount=19000
     private final String SECRET_KEY = "test_sk_qLlDJaYngro2Bjq0Y2KVezGdRpXx";
-
     @RequestMapping("/order/success")
     public String confirmPayment(
             @RequestParam String paymentKey, @RequestParam String orderId, @RequestParam Long amount,
             Model model) throws Exception {
-        log.info("paymentKey={}",paymentKey);
-        log.info("orderId={}",orderId);
-        log.info("amount={}",amount);
         HttpHeaders headers = new HttpHeaders();
-        // headers.setBasicAuth(SECRET_KEY, ""); // spring framework 5.2 이상 버전에서 지원
         headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -109,21 +103,29 @@ public class OrderController {
         ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(
                 "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
 
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            log.info("결제성공");
-            JsonNode successNode = responseEntity.getBody();
-            model.addAttribute("orderId", successNode.get("orderId").asText());
-            String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
+        if (responseEntity.getStatusCode() == HttpStatus.OK) { //결제성공
+
+            String [] temp = orderId.split("-");
+            Long savedOrderId = Long.parseLong(temp[0]);
+
+            //order status 변경
+            //item sellstatus 변경
+            OrderDto orderDto = orderService.changeStatus(savedOrderId);
+
+            //장바구니 비우기
+            cartService.cartRemoveAll(orderDto.getUser().getId());
+
             return "success";
         } else {
             log.info("결제실패");
             JsonNode failNode = responseEntity.getBody();
+
             model.addAttribute("message", failNode.get("message").asText());
             model.addAttribute("code", failNode.get("code").asText());
+
             return "fail";
         }
     }
-
 
     @RequestMapping("/order/fail")
     public String failPayment(@RequestParam String message, @RequestParam String code, Model model) {
--- src/test/java/com/proj/KnitMarket/Service/OrderServiceTest.java
+++ src/test/java/com/proj/KnitMarket/Service/OrderServiceTest.java
@@ -1,5 +1,7 @@
 package com.proj.KnitMarket.Service;
 
+import com.proj.KnitMarket.Constant.OrderStatus;
+import com.proj.KnitMarket.Constant.SellStatus;
 import com.proj.KnitMarket.domain.Order.OrderItemRepository;
 import com.proj.KnitMarket.domain.Order.OrderRepository;
 import com.proj.KnitMarket.dto.OrderDto;
@@ -22,11 +24,11 @@ class OrderServiceTest {
     @Autowired
     private OrderRepository orderRepository;
 
-    @BeforeEach
+    /*@BeforeEach
     void clean(){
         orderItemRepository.deleteAll();
         orderRepository.deleteAll();
-    }
+    }*/
 
     @Test
     @DisplayName("단일주문정보입력")
@@ -52,6 +54,20 @@ class OrderServiceTest {
         OrderDto orderDto = orderService.orders(userId);
 
         //then
+
+    }
+
+    @Test
+    @DisplayName("결제 후 item status 변경테스트 ")
+    void test3(){
+        //given
+        Long orderId = 229L;
+        //when
+        OrderDto orderDto = orderService.changeStatus(orderId);
+        //then
+        //assertEquals("SOLD_OUT",orderDto.getOrderItems().get(0).getItem().getSellStatus());
+        //assertEquals(OrderStatus.ORDER,orderDto.getOrderStatus());
+
 
     }
 
