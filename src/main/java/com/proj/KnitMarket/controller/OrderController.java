15a14a494617043b799eda5fe8ecc3e4c20787f4
HAN-SEOHYUN <000312>
2022-09-07 23:59:57 +0900

    tossPayments 파라미터 설정 ok

--- src/main/java/com/proj/KnitMarket/controller/OrderController.java
+++ src/main/java/com/proj/KnitMarket/controller/OrderController.java
@@ -88,11 +88,13 @@ public class OrderController {
     //http://localhost:8086/success?orderId=1234567&paymentKey=vG45eDbZnodP9BRQmyarYBdE4boBL8J07KzLNkE6AOMwXYWl&amount=19000
     private final String SECRET_KEY = "test_sk_qLlDJaYngro2Bjq0Y2KVezGdRpXx";
 
-    @RequestMapping("/success")
+    @RequestMapping("/order/success")
     public String confirmPayment(
             @RequestParam String paymentKey, @RequestParam String orderId, @RequestParam Long amount,
             Model model) throws Exception {
-
+        log.info("paymentKey={}",paymentKey);
+        log.info("orderId={}",orderId);
+        log.info("amount={}",amount);
         HttpHeaders headers = new HttpHeaders();
         // headers.setBasicAuth(SECRET_KEY, ""); // spring framework 5.2 이상 버전에서 지원
         headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
@@ -108,11 +110,13 @@ public class OrderController {
                 "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            log.info("결제성공");
             JsonNode successNode = responseEntity.getBody();
             model.addAttribute("orderId", successNode.get("orderId").asText());
             String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
             return "success";
         } else {
+            log.info("결제실패");
             JsonNode failNode = responseEntity.getBody();
             model.addAttribute("message", failNode.get("message").asText());
             model.addAttribute("code", failNode.get("code").asText());
@@ -121,11 +125,11 @@ public class OrderController {
     }
 
 
-    @RequestMapping("/fail")
+    @RequestMapping("/order/fail")
     public String failPayment(@RequestParam String message, @RequestParam String code, Model model) {
         model.addAttribute("message", message);
         model.addAttribute("code", code);
-        return "fail";
+        return "redirect:/";
     }
 
 
