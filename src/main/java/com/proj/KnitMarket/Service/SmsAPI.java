89c9d8f693ebd2eb53ecabaf23f50306b3ccf0c7
HAN-SEOHYUN <000312>
2022-10-02 15:51:02 +0900

    결제완료 시 관리자에게 sms 보내기 설정

--- src/main/java/com/proj/KnitMarket/Service/SmsAPI.java
+++ src/main/java/com/proj/KnitMarket/Service/SmsAPI.java
@@ -12,7 +12,7 @@ import net.nurigo.java_sdk.exceptions.CoolsmsException;
 @Service
 public class SmsAPI {
 
-    public void sendSms(String hp, int orderNo, String name, int totalPrice) throws CoolsmsException {
+    public void sendSms(String hp, Long orderNo, String name, int totalPrice) throws CoolsmsException {
 
         String api_key = "NCS7YOD372XJHYT6";
         String api_secret = "9VQKF869G8FVNFIYLPRBFB1GOH2NTZ2I";
@@ -22,7 +22,7 @@ public class SmsAPI {
         params.put("to", hp);
         params.put("from", "01049012186");
         params.put("type", "SMS");
-        params.put("text", "[뜨개장터] " + " 주문번호: " + orderNo + ", " + name + "님, " + totalPrice + "원 결제완료되었습니다. ~일 내에 발송됩니다");
+        params.put("text", "[뜨개장터] " + " 주문번호: " + orderNo + ", " + name + "님, " + totalPrice + "원 결제완료되었습니다. 구매하신 상품은 3일 내에 발송됩니다");
         params.put("app_version", "test app 1.2");
 
         try {
