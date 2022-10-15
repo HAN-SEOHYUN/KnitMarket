845a98f3e7f80e2bb34da758f4c3a776bc4394d5
HAN-SEOHYUN <000312@daum.net>
2022-10-16 01:15:35 +0900

    네이버 로그인 SELLER

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    //카카오 로그인 버튼 클릭시
     $('#kakaoLoginBtn').click(function(){
         var role = "";
         if ($('#checkbox_user').is(":checked")) {
@@ -11,9 +13,27 @@ $(function () {
             "&redirect_uri=http://knitmarket.shop/kakaoLogin/requestToken_" + role +
         "&response_type=code";
         location.href = kakaoUrl;
+    });
+
+    //네이버 로그인 버튼 클릭시
+    $('#naverLoginBtn').click(function(){
+        var role = "";
+        if ($('#checkbox_user').is(":checked")) {
+            role = "user";
+        } else {
+            role = "seller";
+        }
+        var kakaoUrl = "https://nid.naver.com/oauth2.0/authorize?" +
+            "response_type=code" +
+            "&client_id=AU7orLrqqxELyBf6vaPd" +
+            "&redirect_uri=http://localhost:8080/naverLogin/requestToken_" + role +
+            "&state=STATE_STRING";
+        location.href = kakaoUrl;
     });//한개만 선택되게하기
+
 });
 
+//한개만 선택되게
 function checkOnlyOne(element) {
 
     const checkboxes
